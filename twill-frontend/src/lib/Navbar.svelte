4acfe4dbf99ff280fb722f81fd03e3b654e1fd7b
Rami <rami.awar.ra@gmail.com>
2022-08-03 13:17:39 +0300

    Ignore - Modified Navbar

--- twill-frontend/src/lib/Navbar.svelte
+++ twill-frontend/src/lib/Navbar.svelte
@@ -80,7 +80,7 @@
 				<div class="hidden sm:ml-6 sm:flex sm:space-x-8">
 					<!-- Current: "", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
 					<a
-						href="#"
+						href="/tweets/123"
 						class="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
 					>
 						Dashboard
