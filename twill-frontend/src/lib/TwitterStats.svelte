39e17a819fa0dacfe7706024b43cdf40895780eb
Rami <rami.awar.ra@gmail.com>
2022-08-09 10:52:09 +0300

    feat(dashboard): Added some stats components

---
+++ twill-frontend/src/lib/TwitterStats.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Stats from '$lib/Stats.svelte';
+</script>
+
+<div>
+	<h3 class="text-lg leading-6 font-bold text-gray-900">
+		Today <span class="text-sm text-gray-500 ml-4 font-normal">Compared to 14 day averages</span>
+	</h3>
+
+	<dl
+		class="mt-5 grid grid-cols-1 rounded-lg bg-white overflow-hidden shadow divide-y divide-gray-200 md:grid-cols-4 md:divide-y-0 md:divide-x"
+	>
+		<Stats title="Followers" previousValue="1255" />
+		<Stats title="Followers" />
+		<Stats title="Followers" />
+		<Stats title="Followers" />
+	</dl>
+</div>
--- twill-frontend/src/routes/dashboard.svelte
+++ twill-frontend/src/routes/dashboard.svelte
@@ -1,5 +1,19 @@
+<script context="module">
+	export function load({ session }) {
+		if (!session.user_id) {
+			return {
+				status: 302,
+				redirect: `/`
+			};
+		}
+
+		return {};
+	}
+</script>
+
 <script>
 	import ExampleBarChart from '$lib/ExampleBarChart.svelte';
+	import TwitterStats from '$lib/TwitterStats.svelte';
 </script>
 
 <div class="py-10">
@@ -9,6 +23,22 @@
 		</div>
 	</header>
 	<main>
+		<div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+			<!-- Replace with your content -->
+			<div class="px-4 py-8 sm:px-0">
+				<TwitterStats />
+			</div>
+			<!-- /End replace -->
+		</div>
+
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<!-- Replace with your content -->
+			<div class="px-4 py-8 sm:px-0">
+				<ExampleBarChart />
+			</div>
+			<!-- /End replace -->
+		</div>
+
 		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 			<!-- Replace with your content -->
 			<div class="px-4 py-8 sm:px-0">
