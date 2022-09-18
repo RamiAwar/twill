09576c478bba4e94256ae3cd5762eb1b34750420
Rami <rami.awar.ra@gmail.com>
2022-09-18 23:46:16 +0300

    build(nuxt): Removed dockerfile, added potential deploy fid

--- twill-nuxt/nuxt.config.ts
+++ twill-nuxt/nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from "nuxt/config";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  alias: {
+    "class-validator": "class-validator/cjs/index.js",
+  },
   buildModules: ["@nuxtjs/tailwindcss"],
   modules: ["@pinia/nuxt"],
   plugins: ["~/plugins/persistedstate.ts"],
